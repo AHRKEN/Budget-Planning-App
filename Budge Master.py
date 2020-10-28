ae332f10ed485acb0c6185f6d3c686a229e6c1b7
Estalin <estalin.eml7@gmail.com>
2020-10-27 22:11:32 -0600

    Update Budge Master.py

--- Budge Master.py
+++ Budge Master.py
@@ -1,5 +1,5 @@
 """
-Main file for the Budge Master App.
+Main file for the Budge Master App..
 This is an app for budget management.
 *** This program needs a 64-bit Python interpreter for ram memory advantage and optimization purposes.
 All the print statements are being used just in the current developing phase for testing and
